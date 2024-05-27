f50330a3ecf1f43005e5a3d63c4390a9fb7fb16d
dyun <zkdlxkxks@nate.com>
2024-05-27 20:50:31 +0900

    add: train scripts

---
+++ scripts/train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+env_name=("halfcheetah" "hopper" "walker2d")
+dataset=('medium' 'medium-replay' 'medium-expert')
+batch_size=(32 64)
+max_iter=600
+
+for env in "${env_name[@]}"; do
+    for ds in "${dataset[@]}"; do
+        for bs in "${batch_size[@]}"; do
+            echo "Running train script: $env-$ds batch_size=$batch_size max_iter=$max_iter"
+            python transformer/gpt_transformer/train/train_transFOLD.py --env_name $env_name --dataset $dataset --batch_size $bs --max_train_iters $max_iter
+        done
+    done
+done
