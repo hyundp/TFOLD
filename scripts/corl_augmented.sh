98d6c658b8f409a4d345f30d03fe02420efbd35c
dyun <zkdlxkxks@nate.com>
2024-05-31 15:23:07 +0900

    add: corl scripts

---
+++ scripts/corl_augmented.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+env_name=("halfcheetah" "hopper" "walker2d")
+dataset=('medium' 'medium-replay' 'medium-expert')
+seed=(0 1 2)
+
+for env in "${env_name[@]}"; do
+    for ds in "${dataset[@]}"; do
+        for s in "${seed[@]}"; do
+            echo "Running corl_augmented script: $env-$ds"
+            python corl/algorithms/td3_bc.py --env_name $env --dataset $ds --seed $s
+        done
+    done
+done
