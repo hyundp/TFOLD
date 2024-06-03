3594e3b182653bedaf96201004900aafba42e276
dyun <zkdlxkxks@nate.com>
2024-06-03 23:16:36 +0900

    add: original args

--- scripts/corl_original.sh
+++ scripts/corl_original.sh
@@ -6,6 +6,6 @@ dataset=('medium' 'medium-replay' 'medium-expert')
 for env in "${env_name[@]}"; do
     for ds in "${dataset[@]}"; do
         echo "Running corl_original script: $env-$ds"
-        python corl/algorithms/td3_bc.py --env_name $env --dataset $ds
+        python corl/algorithms/td3_bc.py --env_name $env --dataset $ds --original
     done
 done
